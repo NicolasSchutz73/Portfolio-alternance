56a3bb838b5984f86bf75ade2175edf8be2495ea
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-04-03 18:56:39 +0200

    Initial commit

---
+++ resources/js/Pages/Test.vue
@@ -0,0 +1,16 @@
+<script setup>
+const name = 'Test';
+</script>
+
+<template>
+    <div class="container">
+        <h1>Hello World {{ name }}</h1>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
